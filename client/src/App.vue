e29906eacb970e44905be9b102f52d3e0690ee5f
jtzhangmy <aspirine839716326@gmail.com>
2016-12-23 18:58:28 +0800

    添加登录登出消息逻辑

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <div class="member-num">当前在线 人</div>
+    <div class="member-num">当前在线{{userNum}}人</div>
 
     <div class="message">
       <div class="content">
         <div class="content-inner"></div>
       </div>
       <div class="sidebar">
-
+        <div class="user-list" v-for="userItem in userList">
+          {{userItem}}
+        </div>
       </div>
     </div>
 
@@ -17,12 +19,19 @@
           <div class="tool-bar">
 
           </div>
-          <textarea class="textarea" cols="50" rows="10"></textarea>
+          <textarea class="textarea" cols="50" rows="10" v-model="editingMsg" @keyup.enter="send()"></textarea>
         </div>
         <div class="button-bar">
-          <button class="send">发送</button>
+          <button class="send" @click="send()">发送</button>
+          <button class="empty" @click="empty()">清空</button>
         </div>
       </div>
+    </div>
+
+    <div class="input-username" v-if="inputName">
+      <h3 class="username-title">请输入用户名</h3>
+      <input class="username-input" type="text" v-model="username" @keydown.enter="saveUsername">
+      <button class="username-button" @click="saveUsername()">确认</button>
     </div>
   </div>
 </template>
@@ -32,9 +41,86 @@ export default {
   name: 'app',
   data () {
     return {
-      userNum: '',
-      userList: [],
-      messageList: []
+      inputName: true,//编辑用户名
+      username: '',//用户名
+      userNum: '0',//在线用户数量
+      fontSize: '12px',
+      color: '#000',
+      ws: '',//websocket
+      userList: [],//用户列表
+      messageList: [],//消息列表
+      editingMsg: ''//输入框中的消息
+    }
+  },
+  mounted: function () {
+    this.initWebSocket();
+  },
+  methods: {
+    initWebSocket: function () {
+      var vm = this;
+      vm.ws = new WebSocket("ws://10.10.33.19:8081");
+      vm.ws.onopen = function (e) {
+        console.log('websocket 链接服务端成功');
+      };
+
+      vm.ws.onmessage = function (msg) {
+        var msgData = JSON.parse(msg.data);
+        var type = msgData.type;
+        switch (type) {
+          //用户登录
+          case 'userLogin':
+            vm.userNum = msgData.userNum;
+              console.log(msgData.userNum);
+            break;
+          //用户列表
+          case 'list':
+            vm.userList = msgData.userList;
+            break;
+          //接收消息
+          case 'message':
+
+            break;
+          //用户退出
+          case 'logout':
+            console.log(msgData);
+            break;
+          //退出轮询
+          case 'test':
+              var testData = {
+                type: 'test',
+                username: vm.username
+              };
+            vm.ws.send(JSON.stringify(testData));
+            break;
+        }
+      }
+    },
+    saveUsername: function () {
+      this.$data.inputName = false;
+      var data = {
+        type: 'userLogin',
+        username: this.$data.username
+      };
+      //发送消息
+      this.$data.ws.send(JSON.stringify(data));
+    },
+    send: function () {
+      var vm = this;
+      var msgData = {
+        type: 'message',
+        user: vm.username,
+        userHeader: '',
+        ctx: vm.editingMsg,
+        fontSize: vm.fontSize,
+        color: vm.color
+      };
+      vm.ws.send(JSON.stringify(msgData));
+      vm.editingMsg = '';
+      var textarea = document.getElementsByClassName('textarea')[0];
+      textarea.focus();
+    },
+    empty: function () {
+      this.$data.editingMsg = '';
     }
   }
 }
@@ -44,6 +130,7 @@ export default {
   * {
     box-sizing: border-box;
   }
+
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -90,6 +177,13 @@ export default {
     border-radius: 5px;
   }
 
+  .user-list {
+    width: 100%;
+    height: 20px;
+    padding-left: 10px;
+    text-align: left;
+  }
+
   .foot-bar {
     width: 60%;
     height: 150px;
@@ -133,6 +227,18 @@ export default {
     margin-top: 10px;
   }
 
+  .empty {
+    width: 60px;
+    height: 30px;
+    float: right;
+    border: none;
+    border-radius: 5px;
+    background: #ccc;
+    color: #fff;
+    font-size: 14px;
+    outline: none;
+  }
+
   .send {
     width: 60px;
     height: 30px;
@@ -142,6 +248,44 @@ export default {
     background: #00a1d6;
     color: #fff;
     font-size: 14px;
+    outline: none;
+    margin-left: 20px;
+  }
+
+  .input-username {
+    width: 300px;
+    height: 150px;
+    border: 1px solid #ddd;
+    background: #fff;
+    border-radius: 5px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -75px;
+    margin-left: -150px;
+  }
+
+  .username-input {
+    width: 180px;
+    height: 30px;
+    line-height: 30px;
+    padding-left: 10px;
+    font-size: 16px;
+    border-radius: 5px;
+    margin: 0 auto;
+    display: block;
+  }
+
+  .username-button {
+    width: 80px;
+    height: 30px;
+    margin-top: 10px;
+    border: none;
+    border-radius: 5px;
+    background: #00a1d6;
+    color: #fff;
+    font-size: 16px;
+    font-weight: 700;
   }
 
 </style>
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,103 @@
-var router = require('koa-router')()
+var app = require('koa')()
+  , router = require('koa-router')()
   , ws = require('ws');
+
+var sever = app.listen(8081);
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({
+  server: sever
+});
+
+var userList = [];
+var msgNum = 0;
+
+Array.prototype.remove = function(val) {
+  var index = this.indexOf(val);
+  if (index > -1) {
+    this.splice(index, 1);
+  }
+};
+
+wss.on('connection', function (ws) {
+  console.log("在线人数", wss.clients.length);
+  console.log('----链接成功----');
+
+  ws.on('message', function (msg) {
+    var message = JSON.parse(msg);
+    console.log('====================');
+    console.log(message);
+    var msgType = message.type;
+    if(msgType == 'userLogin') {//用户登录
+      var username = message.username;
+      userList.push(username);
+      var loginData = {
+        type: 'userLogin',
+        userNum: wss.clients.length,
+        userName: username
+      };
+      wss.broadcast(loginData);
+
+      var listData = {
+        type: 'list',
+        userList: userList
+      };
+      console.log(listData);
+      ws.send(JSON.stringify(listData));
+    } else if (msgType == 'message') {//接收消息
+      // var msgData = JSON
+      var dateTime = Date.parse(new Date());
+      message.dateTime = dateTime;
+      wss.broadcast(message);
+    } else if (msgType == 'test') {
+      var leaveUser = userList;
+      msgNum --;
+      console.log('+++++++++++++++');
+      console.log(msgNum);
+      if (msgNum == 0) {
+        leaveUser.remove(message.username);
+        var logoutData = {
+          type: 'logout',
+          userName: leaveUser[0]
+        };
+        wss.broadcast(logoutData);
+      } else {
+        leaveUser.remove(message.username);
+      }
+    }
+
+  });
+
+  ws.on('close', function (close) {
+    console.log("用户退出,在线人数" + wss.clients.length);
+    var testData = {
+      type: 'test'
+    };
+    wss.leaveBroadcast(testData);
+
+
+  });
+});
+
+//为wss对象添加广播方法
+wss.broadcast = function (data) {
+  console.log('--------------广播---------------');
+  console.log(data);
+  wss.clients.forEach(function (client) {
+    client.send(JSON.stringify(data));
+  });
+};
+
+wss.leaveBroadcast = function (data) {
+  console.log('--------------退出广播---------------');
+  wss.clients.forEach(function (client) {
+    client.send(JSON.stringify(data));
+    msgNum ++;
+    console.log(msgNum);
+  });
+};
 
 router.get('/', function *(next) {
   var ctx = this;
-  
 
 });
 
